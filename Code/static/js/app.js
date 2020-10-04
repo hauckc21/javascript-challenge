688d732ad14fe7d04cc0e1939226807c94ca8b16
hauckc21 <hauck.chris@gmail.com>
2020-10-04 13:13:40 -0500

    filter redo

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -1,15 +1,11 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-console.log(tableData);
-
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
 // UFO Sighting values for each column
 tableData.forEach(function(ufoSighting) {
-    console.log(ufoSighting);
     // Append one table row `tr` for each UFO Sighting object
     var row = tbody.append("tr");
 
