2a20b587d88af1331ed596fec8ae665d73026e46
hauckc21 <hauck.chris@gmail.com>
2020-10-05 19:35:37 -0500

    log issues

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -1,22 +1,25 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
 // UFO Sighting values for each column
 tableData.forEach(function(ufoSighting) {
-    // Append one table row for each UFO Sighting object
+    console.log(ufoSighting);
+    // Append one table row `tr` for each UFO Sighting object
     var row = tbody.append("tr");
 
     // Use `Object.entries` to console.log each UFO Sighting value
     Object.entries(ufoSighting).forEach(function([key, value]) {
       console.log(key, value);
-      // Append a cell for each value
+      // Append a cell to the row for each value
       var cell = row.append("td");
       cell.text(value);
     });
   });
+
 
 // Select the button
 var button = d3.select("#filter-btn");
@@ -26,22 +29,27 @@ button.on("click", function() {
 
     // Select the input date get the raw HTML nodes
     var inputElement = d3.select("#datetime");
-    // Get the value property of the input element
+    // Get the value property of the input date, state, shape
     var inputValue = inputElement.property("value");
+    // console.log input value
+    console.log(inputValue);
     // Filter Data with datetime equal to input value
     var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    // console.log filter values
+    console.log(filteredData);
 
 
     filteredData.forEach(function(selections) {
 
-      // Append a table row for each UFO Sighting object
-      var row = tbody.append("tr");
-      // Use `Object.entries` to console.log each UFO Sighting value
-      Object.entries(selections).forEach(function([key, value]) {
-          console.log(key, value);
-          // Append a cell to the row for each value
-          var cell = row.append("td");
-          cell.text(value);
-      });
-  });
-  });
+    console.log(selections);
+    // Append one table row `tr` for each UFO Sighting object
+    var row = tbody.append("tr");
+    // Use `Object.entries` to console.log each UFO Sighting value
+    Object.entries(selections).forEach(function([key, value]) {
+        console.log(key, value);
+        // Append a cell to the row for each value
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+});
