9618cd43518d816b3b87c99ba90dfd31f1a7d6b0
hauckc21 <hauck.chris@gmail.com>
2020-10-05 19:40:59 -0500

    Final Version

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -8,19 +8,20 @@ var tbody = d3.select("tbody");
 // UFO Sighting values for each column
 tableData.forEach(function(ufoSighting) {
     console.log(ufoSighting);
-    // Append one table row `tr` for each UFO Sighting object
+    // Append one table row `tr` to the table body
     var row = tbody.append("tr");
 
     // Use `Object.entries` to console.log each UFO Sighting value
     Object.entries(ufoSighting).forEach(function([key, value]) {
       console.log(key, value);
-      // Append a cell to the row for each value
+      // Append one cell to the row for each value
       var cell = row.append("td");
       cell.text(value);
     });
   });
 
 
+  
 // Select the button
 var button = d3.select("#filter-btn");
 button.on("click", function() {
@@ -42,12 +43,12 @@ button.on("click", function() {
     filteredData.forEach(function(selections) {
 
     console.log(selections);
-    // Append one table row `tr` for each UFO Sighting object
+    // Append one table row `tr` to the table body
     var row = tbody.append("tr");
     // Use `Object.entries` to console.log each UFO Sighting value
     Object.entries(selections).forEach(function([key, value]) {
         console.log(key, value);
-        // Append a cell to the row for each value
+        // Append one cell to the row for each value
         var cell = row.append("td");
         cell.text(value);
     });
